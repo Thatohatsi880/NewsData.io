761aeff40d20ba100999a795c066c50d32680416
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-05-19 00:32:35 +0200

    Added variables to the javascript page

---
+++ index.js
@@ -0,0 +1,37 @@
+//variables
+const generalBtn = document.getElementById("genral");
+const businessBtn = document.getElementById("business");
+const sportsBtn = document.getElementById("sport");
+const entertainmentBtn = document.getElementById("entertainment");
+const technologyBtn = document.getElementById("technology");
+const searchBtn = document.getElementById("searchBtn");
+
+
+const newsQuery = document.getElementById("newsQuery");
+const newsType = document.getElementById("newsType");
+const newsdetails = document.getElementById("newsdtails");
+//apis
+
+generalBtn.addEventListener("click", function(){
+
+});
+
+businessBtn.addEventListener("click", function(){
+
+});
+
+sportsBtn.addEventListener("click", function(){
+
+});
+
+entertainmentBtn.addEventListener("click", function(){
+
+});
+
+technologyBtn.addEventListener("click", function(){
+
+});
+
+searchBtn.addEventListener("click", function(){
+
+});
